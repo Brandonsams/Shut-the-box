c20842104bdab1984146292b526b54d854992bff
brandonsams <brandonkeithsams@gmail.com>
2024-02-03 16:57:13 -0700

    added a frequency counter for analytics

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from random import randint
 from itertools import combinations
 from tqdm import tqdm
 
+num_dice = 2
+num_sides_per_die = 6
+success_count = 0
+total_trial_count = 1_000_000
 
 class Die:
     def __init__(self, num_sides=6):
@@ -29,6 +33,10 @@ class Box:
             rv += die.roll()
         self.roll_history.append(rv)
         return rv
+
+    def get_roll_count(self):
+        roll_count = len(self.roll_history)
+        return roll_count
 
     def get_valid_subsets(self, dice_total):
         rv = []
@@ -68,11 +76,12 @@ class Box:
             rv += f"{box_state} --- {roll}\n"
         return rv
 
+roll_count_frequency_success = {}
+roll_count_frequency_failure = {}
+for roll_count in range(1, num_dice * num_sides_per_die + 1):
+    roll_count_frequency_success[roll_count] = 0
+    roll_count_frequency_failure[roll_count] = 0
 
-num_dice = 2
-num_sides_per_die = 6
-success_count = 0
-total_trial_count = 100_000
 
 for i in tqdm(range(total_trial_count)):
     box = Box(num_dice=num_dice, num_sides_per_die=num_sides_per_die)
@@ -84,10 +93,15 @@ for i in tqdm(range(total_trial_count)):
         #     break
         first_subset = box.get_first_valid_subset(dice_total=dice_total)
         if first_subset is None:
+            roll_count_frequency_failure[box.get_roll_count()] += 1
             break
         box.remove_numbers(first_subset)
     else:
         # print(repr(box))
+        roll_count_frequency_success[box.get_roll_count()] += 1
         success_count += 1
 
 print(f"({num_dice},{num_sides_per_die}): {success_count} / {total_trial_count} = {100*(1.0 *success_count/total_trial_count):.4f}%")
+for roll_count in range(1, num_dice * num_sides_per_die + 1):
+    print(
+        f"{roll_count}, {roll_count_frequency_success[roll_count]}, {roll_count_frequency_failure[roll_count]}")
